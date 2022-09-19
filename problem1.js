9a715c32594e5602a66fbb3d185a933621f5ba44
unknown <Sutarimran47@gmail.com>
2022-09-19 22:09:18 +0530

    logic_functionality

---
+++ problem1.js
@@ -0,0 +1,11 @@
+let x=13;
+count=0;
+for(i=1;i<=x;i++){
+if(x%i==0){
+count++;
+}
+}if(count==1){
+console.log("its a prime")
+}else{
+console.log("its not a prime")
+}
---
+++ problem2.js
@@ -0,0 +1,10 @@
+let x=naman;
+bag="";
+for(i=x.length-1;i>=0;i++){
+bag=bag+x[i];
+}
+if(x==bag){
+console.log("Palindrome")
+}else{
+console.log("not Palindrome")
+}
